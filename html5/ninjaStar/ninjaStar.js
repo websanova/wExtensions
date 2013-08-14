390f50d8863cd8a68409ca26bcc2c3bbab991816
websanova <rob@websanova.com>
2013-08-14 14:11:13 +0700

    Add ninjaStar.js

---
+++ html5/ninjaStar/ninjaStar.js
@@ -0,0 +1,16 @@
+CanvasRenderingContext2D.prototype.ninjaStar = function(x, y, width, height) {
+  // if values are not set just exit
+  if(!x || !y || !width || !height) { return true; }
+
+  this.beginPath();
+  this.moveTo(x + width*0.5, y);
+  this.lineTo(x + width*0.35, y + height*0.35);
+  this.lineTo(x, y + height*0.5);
+  this.lineTo(x + width*0.35, y + height*0.65);
+  this.lineTo(x + width*0.5, y + height);
+  this.lineTo(x + width*0.65, y + height*0.65);
+  this.lineTo(x + width, y +height*0.5);
+  this.lineTo(x + width*0.65, y + height*0.35);
+  this.lineTo(x + width*0.5, y);
+  this.closePath();  
+};
